5ed94a514af32bd75c0d973c0f0f8d9de344e9cb
unknown <littlegruz@gmail.com>
2012-06-11 13:51:45 +1000

    Added permissions, messages; fixed cowsay messages

--- src/littlegruz/cowsay/commands/Cowsay.java
+++ src/littlegruz/cowsay/commands/Cowsay.java
@@ -9,7 +9,7 @@ import org.spout.api.command.CommandSource;
 import org.spout.api.exception.CommandException;
 
 public class Cowsay implements CommandExecutor{
-private CowsayMain plugin;
+   private CowsayMain plugin;
    
    public Cowsay(CowsayMain instance){
       plugin = instance;
@@ -19,44 +19,50 @@ private CowsayMain plugin;
    public boolean processCommand(CommandSource source, Command cmd,
          CommandContext args) throws CommandException{
       if(cmd.getPreferredName().compareToIgnoreCase("cowsay") == 0){
-         String output, name;
-         
-         output = "";
-         name = source.getName() + ": ";
-         
-         if(args.length() > 0){
-            output = makeMessage(1, output, args);
-            source.sendMessage(args.getString(0));
+         if(source.hasPermission("mccowsay.cowsay")){
+            String output, name;
             
-            if(args.getString(0).compareToIgnoreCase("head-in") == 0)
-               plugin.printHeadIn(name.replace(": ", ""), output);
-            else if(args.getString(0).compareToIgnoreCase("moose") == 0)
-               plugin.printMoose(name, output);
-            else if(args.getString(0).compareToIgnoreCase("moofasa") == 0)
-               plugin.printMoofasa(name, output);
-            else if(args.getString(0).compareToIgnoreCase("elephant") == 0)
-               plugin.printElephant(name, output);
-            else if(args.getString(0).compareToIgnoreCase("udder") == 0)
-               plugin.printUdder(name, output);
-            else if(args.getString(0).compareToIgnoreCase("tux") == 0)
-               plugin.printTux(name, output);
-            else if(args.getString(0).compareToIgnoreCase("tiny") == 0)
-               plugin.printTiny(name, output);
-            else{
-               output = "";
-               output = makeMessage(0, output, args);
-               plugin.printCow(name, output);
+            output = "";
+            name = source.getName() + ": ";
+            
+            // Check the number of arguments used
+            if(args.length() > 0){
+               output = makeMessage(1, output, args);
+               
+               // Checks if user specified a cow type
+               if(args.getString(0).compareToIgnoreCase("head-in") == 0)
+                  plugin.printHeadIn(name.replace(": ", ""), output);
+               else if(args.getString(0).compareToIgnoreCase("moose") == 0)
+                  plugin.printMoose(name, output);
+               else if(args.getString(0).compareToIgnoreCase("moofasa") == 0)
+                  plugin.printMoofasa(name, output);
+               else if(args.getString(0).compareToIgnoreCase("elephant") == 0)
+                  plugin.printElephant(name, output);
+               else if(args.getString(0).compareToIgnoreCase("udder") == 0)
+                  plugin.printUdder(name, output);
+               else if(args.getString(0).compareToIgnoreCase("tux") == 0)
+                  plugin.printTux(name, output);
+               else if(args.getString(0).compareToIgnoreCase("tiny") == 0)
+                  plugin.printTiny(name, output);
+               else{
+                  output = "";
+                  output = makeMessage(0, output, args);
+                  plugin.printCow(name, output);
+               }
             }
-            
+            else
+               source.sendMessage("Needs more arguments. Try \'/cowsay [cow_type] message\'");
          }
          else
-            source.sendMessage("Noooope. Chuck Testa");
+            source.sendMessage("You do not have the required permissions");
       }
       return false;
    }
    
-   private String makeMessage(int i, String output, CommandContext args){
-      for(i = 0; i < args.length(); i++){
+   private String makeMessage(int start, String output, CommandContext args){
+      int i;
+      
+      for(i = start; i < args.length(); i++){
          output = output + args.getString(i) + " ";
       }
       
